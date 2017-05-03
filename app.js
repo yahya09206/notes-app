671ddbb7f5d70151f05e62a6afa6e8797da128fd
Yahya Hussein <yahyahussein91@gmail.com>
2017-05-02 20:54:51 -0700

    fixed error where external file wasnt being read

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ console.log("Starting app.js");
 
 const fs = require('fs');
 const os = require('os');
-const notes = require('./')
+const notes = require('./notes.js');
 
 var user = os.userInfo();
 
