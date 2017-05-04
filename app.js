984180b7c1132aba989d5abfb7820049438a2fd4
Yahya Hussein <yahyahussein91@gmail.com>
2017-05-03 21:17:07 -0700

    required lodash in app.js file

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ console.log("Starting app.js");
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
 console.log('Result:', notes.add(9,-2));
