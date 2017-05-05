82a092a1c3a6febecefde233f0570aea91647b75
Yahya Hussein <yahyahussein91@gmail.com>
2017-05-05 14:35:57 -0700

    added read and delete command to statements

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ if(command === 'add'){
 	console.log('Adding new note!');
 }else if(command === 'list'){
 	console.log('Listing all notes');
+}else if(command === 'Read'){
+	console.log('Fetching note!');
+}else if(command === 'Delete'){
+	console.log('Deleting note!');
 }else{
 	console.log('Command not recognized');
 }
