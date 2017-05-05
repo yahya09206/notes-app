4ce648c42bad41c2413e7f517bf05ac503e994e2
Yahya Hussein <yahyahussein91@gmail.com>
2017-05-05 14:31:37 -0700

    added process object

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 console.log("Starting app.js");
 
+//Third party packages
 const fs = require('fs');
 const _ = require('lodash');
 
+//Local packages
 const notes = require('./notes.js');
+
+//Argv=arguments array
+console.log(process.argv)
