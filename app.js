b99c5a868b6f420f7132a7aafa5424aab63b93ad
Yahya Hussein <yahyahussein91@gmail.com>
2017-05-05 14:48:34 -0700

    added add function

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ console.log('Yargs', argv);
 //condition to add new note
 if(command === 'add'){
 	console.log('Adding new note!');
+	notes.addNote(argv.title, argv.body);
 }else if(command === 'list'){
 	console.log('Listing all notes');
 }else if(command === 'read'){
